e154b8cccdee3b215f4a1a53972ea8b3e1346eb1
Carson Keller <carsonjkeller@gmail.com>
2020-06-07 08:49:59 -0600

    Added additional routing settings and implemented private routes, protecting certain pages from general/unauthorized access

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-d
 import { ShopContext } from './context/ShopContext';
 import { Home } from './pages/Home';
 import { Register } from './pages/Register';
+import { Shop } from './pages/Shop';
 import { Item } from './pages/Item';
 import { Accounts } from './pages/Accounts';
 import { Inventory } from './pages/Inventory';
-import { loggedIn } from './config/Auth';
-import { adminLoggedIn } from './config/Auth';
+import { loggedIn, adminLoggedIn } from './config/Auth';
 
 function App() {
   const { Provider } = ShopContext;
   const [inventory, setInventory] = useState([]);
   const [selectedItem, selectItem] = useState({});
-  const [retrievedItem, setRetrievedItem] = useState({});
+  const [user, setUser] = useState({});
+  // const [loggedIn, setLoggedIn] = useState(false);
   // const [selectedDate, setSelectedDate] = useState({});
   const getInventory = () => {
     const url = process.env.REACT_APP_INVENTORY_API;
@@ -28,20 +29,69 @@ function App() {
         .catch(err => err);
   };
 
-  console.log("selectedItem._id", selectedItem._id)
+  const getUser = () => {
+    if (localStorage.getItem('user')) {
+      setUser(JSON.parse(localStorage.getItem('user')));
+    };
+  };
+
+  const RegisterRoute = ({ children, ...rest }) => {
+    return (
+      <Route
+        {...rest}
+        render = {({ location }) =>
+          loggedIn() ? (<Redirect to={{ pathname: '/',
+          state: { from: location }}} />) : (children)
+        }
+      />
+    )
+  }
+
+  const AccountRoute = ({ children, ...rest }) => {
+    return (
+      <Route
+        {...rest}
+        render = {({ location }) => 
+          loggedIn() ? (children) : 
+          (<Redirect to={{ pathname: '/', 
+          state: { from: location }}} />)
+        }
+      />
+    );
+  };
+  
+  const InventoryRoute = ({ children, ...rest }) => {
+    return (
+      <Route
+        {...rest}
+        render = {({ location }) => 
+          adminLoggedIn() ? (children) :
+          (<Redirect to={{ pathname: '/',
+          state: { from: location }}} />)
+        }
+      />
+    );
+  };
 
   useEffect(() => {getInventory()}, []);
 
+  useEffect(() => {if (inventory) getUser()}, [])
+
+  // useEffect(() => {if (loggedIn) getUser()}, [])
+
   return (
-    <Provider value={{ inventory, getInventory, selectedItem, selectItem, retrievedItem, setRetrievedItem }}>
+    <Provider value={{ inventory, getInventory, selectedItem, selectItem, user, getUser }}>
       <Router>
         <Switch>
           <Route exact path='/' component={Home} />
-          <Route exact path='/register' component={Register} />
+          <Route exact path='/shop' component={Shop} />
           <Route path={`/item`} component={Item} />
-          <PrivateRoute exact path='/accounts'>
+          <RegisterRoute exact path='/register'>
+            <Register />
+          </RegisterRoute>
+          <AccountRoute exact path='/account'>
             <Accounts />
-          </PrivateRoute>
+          </AccountRoute>
           <InventoryRoute exact path='/inventory'>
             <Inventory />
           </InventoryRoute>
@@ -49,32 +99,41 @@ function App() {
       </Router>
     </Provider>
   );
-}
-
-const PrivateRoute = ({ children, ...rest }) => {
-  return (
-    <Route
-      {...rest}
-      render = {({ location }) => 
-        loggedIn() ? (children) : 
-        (<Redirect to={{ pathname: '/', 
-        state: { from: location }}} />)
-      }
-    />
-  );
 };
 
-const InventoryRoute = ({ children, ...rest }) => {
-  return (
-    <Route
-      {...rest}
-      render = {({ location }) => 
-        adminLoggedIn() ? (children) :
-        (<Redirect to={{ pathname: '/',
-        state: { from: location }}} />)
-      }
-    />
-  );
-};
+// const RegisterRouter = ({ children, ...rest }) => {
+//   return (
+//     <Route
+//       {...rest}
+//       render = {({ location }) => 
+//         }
+//   )
+// }
+
+// const PrivateRoute = ({ children, ...rest }) => {
+//   return (
+//     <Route
+//       {...rest}
+//       render = {({ location }) => 
+//         loggedInCheck() ? (children) : 
+//         (<Redirect to={{ pathname: '/', 
+//         state: { from: location }}} />)
+//       }
+//     />
+//   );
+// };
+
+// const InventoryRoute = ({ children, ...rest }) => {
+//   return (
+//     <Route
+//       {...rest}
+//       render = {({ location }) => 
+//         adminLoggedIn() ? (children) :
+//         (<Redirect to={{ pathname: '/',
+//         state: { from: location }}} />)
+//       }
+//     />
+//   );
+// };
 
 export default App;
